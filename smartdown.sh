528e2c686042370a7185b64bdb0c854ff20651b7
XTEALER <humberto0118@gmail.com>
2019-05-29 10:41:31 -0500

    added date to logs

--- smartdown.sh
+++ smartdown.sh
@@ -2,29 +2,31 @@
 
 intertube=0
 starting=1
-logpath=/var/log/netlog
+logpath=/var/log/smartdown.log
+date +"Date : %d/%m/%Y Time : %H.%M.%S"
 
 if [ $starting -eq 1 ]; then
 	sleep 240
 	((starting = 0))
+	printf "\n\n\n---------------($date)---------------\n\n"
 	echo "beginning ping tests..." >> $logpath
 fi
 
 while true
 do
-	echo "waiting 60 seconds to try again..." >> $logpath
+	echo "waiting 60 seconds to try again... ($date)" >> $logpath
         sleep 60
 	ping -c 3 google.com
         if [ $? -eq 0 ]; then
 		((intertube = 0))
-                echo "ping success: $intertube" >> $logpath
+                echo "ping success ($date): $intertube" >> $logpath
         else
 		((intertube++))
-                echo "ping failed: $intertube" >> $logpath
+                echo "ping failed ($date): $intertube" >> $logpath
         
 		#if intertube is greater than 3 then shuts down
 		if [ $intertube -gt 3 ]; then
-			echo "no network or power, restarting..." >> $logpath
+			echo "no network or power, restarting... ($date)" >> $logpath
 			shutdown -P now
 		fi
 	fi
