7e975115d9a659cc58f10c7ac2608880398a44d5
XTEALER <humberto0118@gmail.com>
2019-05-29 18:54:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #script to install the service and run it, must be made executable (~# chmod +x install.sh)
 
 touch /var/log/smartdown.log
-mkdir ~/.scripts
-cp smartdown.sh ~/.scripts/
+mkdir /lib/smart-down
+cp smartdown.sh /lib/smart-down/
 cp smartdown.service /lib/systemd/system/
-chmod +x ~/.scripts/smartdown.sh
+chmod +x /lib/smart-down/smartdown.sh
 chmod +x /lib/systemd/system/smartdown.service
 systemctl enable smartdown
-systemctl start smartdown
+systemctl restart smartdown
 printf "\n\n\n"
 systemctl status smartdown
 echo "done! Ready to try."
